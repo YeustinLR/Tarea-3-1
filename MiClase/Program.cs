865395afd2f0397cc20209e9c6e90449bf9e9075
yeust <yeust@DESKTOP-RA4H2BP>
2021-10-04 22:40:42 -0600

    Agregar archivos de proyecto.

---
+++ MiClase/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MiClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo miVehiculo= new Vehiculo("Kia","KIC45",2016);
+            //miVehiculo.marca = "Toyota";
+            Console.WriteLine("Marca del vehiculo : " + miVehiculo.marca);
+            Console.WriteLine("Placa : "+miVehiculo.placa);
+            Console.WriteLine("Modelo : "+miVehiculo.modelo);
+        }
+    }
+}
---
+++ MiClase/Vehiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiClase
+{
+    class Vehiculo
+    {
+        //propiedades
+        public string marca;
+        public string placa;
+        public int modelo;
+        //metodos
+        public Vehiculo(string marca,string placa, int modelo)
+        {
+            this.marca = marca;
+            this.placa = placa;
+            this.modelo = modelo;
+        }
+
+    }
+}
